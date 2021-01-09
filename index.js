2e896ec420433f533d305c7ea84002bbda0af43d
Raphael <raphaelior09@gmail.com>
2021-01-09 21:53:30 +0200

    add edge template

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 const path = require('path')
+
 const express = require('express');
+const app = express();
 
-const app = new express();
-app.use(express.static('public'));
+const { config, engine } = require('express-edge');
 
-app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+// Configure Edge if need to
+config({ cache: process.env.NODE_ENV === 'production' });
+
+const port = 4000;
+
+app.use(express.static(__dirname + '/public'));
+app.use(engine);
+app.set('views', __dirname + '/views');
+
+app.get('/', function (req,res) {
+    console.log('rendering...')
+    res.render('index');
 });
 
-app.listen(4000, () =>{
+// app.get('/', function (req,res) {
+//     res.sendFile(path.resolve(__dirname, 'pages/index.html'));
+// });
+
+app.get('/about', function  (req, res) {
+    res.sendFile(path.resolve(__dirname, 'pages/about.html'));
+});
+
+app.get('/contact', function  (req, res) {
+    res.sendFile(path.resolve(__dirname, 'pages/contact.html'));
+});
+
+app.get('/post', function  (req, res) {
+    res.sendFile(path.resolve(__dirname, 'pages/post.html'));
+});
+
+app.listen(port, () =>{
     console.log('App listening on port 4000');
 });
