9f51fd7f7525804711a8956caccce6d57a6fa74b
Raphael <raphaelior09@gmail.com>
2021-02-01 22:52:18 +0200

    post req of new post save to mongoDB

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
-
+const bodyParser = require('body-parser');
+const Post = require('./database/models/Post');
+const app = express();
 
 mongoose.connect('mongodb://localhost/node-blog', {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => 'You are now connected to Mongo!')
@@ -18,6 +19,8 @@ config({ cache: process.env.NODE_ENV === 'production' });
 // Automatically sets view engine and adds dot notation to app.render
 app.use(engine);
 app.set('views', `${__dirname}/views`);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.static('public'));
@@ -40,6 +43,12 @@ app.get('/post', function  (req, res) {
 
 app.get('/posts/new', function (req, res) {
     res.render('create');
+});
+
+app.post('/posts/store', function(req, res){
+    Post.create(req.body, function(err, post){
+        res.redirect('/');
+    });
 });
 
 app.listen(port, () =>{
